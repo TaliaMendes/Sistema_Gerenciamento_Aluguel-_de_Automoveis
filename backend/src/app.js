1a60d54d22afe7c76eebefe244f76817f0bf0b70
TaliaMendes <taliafmendes09@gmail.com>
2025-12-23 21:35:05 -0300

    feat: cria servidor Express

---
+++ backend/src/app.js
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
