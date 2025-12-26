81f77f2025aee207654ded5b9c7004de36caeae9
TaliaMendes <taliafmendes09@gmail.com>
2025-12-26 16:32:51 -0300

    feat: adiciona utilização VeiculosRoutes no arquivo principal

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,16 @@ import 'dotenv/config';
 import express from 'express';
 
 import { initModels } from './models/initModels.js'
+import veiculoRoutes from './routes/veiculo.routes.js';
 
 const app = express();
 app.use(express.json());
 
 initModels();
 console.log('Banco inicializado e tabelas criadas.');
+
+//Rotas 
+app.use(veiculoRoutes);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
