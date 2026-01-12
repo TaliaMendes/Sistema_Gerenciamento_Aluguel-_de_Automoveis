c6bbe2f612f4c2641396c4c2af696c9e1af1a2c3
TaliaMendes <taliafmendes09@gmail.com>
2026-01-12 17:48:58 -0300

    feat: adiciona listagem de reservas do usuário.

--- backend/src/controllers/ReservaController.js
+++ backend/src/controllers/ReservaController.js
@@ -9,3 +9,13 @@ export function criarReserva (req, res, next){
     next(createHttpError(400, err.message));
   }
 }
+
+export function listarReservasUsuarios(req, res, next) {
+  try {
+    const { usuario_id, status } = req.query;
+    const data = ReservaService.listarReservasUsuario(usuario_id, { status });
+    res.json(data);
+  } catch (err) {
+    next(createHttpError(400, err.message));
+  }
+}
--- backend/src/routes/Reserva.routes.js
+++ backend/src/routes/Reserva.routes.js
@@ -4,5 +4,6 @@ import * as ReservaController from '../controllers/ReservaController.js'
 const router = Router();
 
 router.post('/runcar/reservas', ReservaController.criarReserva);
+router.get('/runcar/reservas/usuarios', ReservaController.listarReservasUsuarios);
 
 export default router;
