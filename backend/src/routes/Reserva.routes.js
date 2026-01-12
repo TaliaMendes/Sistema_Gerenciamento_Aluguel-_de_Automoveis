2c8828535fe3bb2aad0bfa6dfbaeb7d607ec24ab
TaliaMendes <taliafmendes09@gmail.com>
2026-01-12 19:48:56 -0300

    feat: finaliza reserva e atualiza status do veículo

--- backend/src/routes/Reserva.routes.js
+++ backend/src/routes/Reserva.routes.js
@@ -7,5 +7,6 @@ router.post('/runcar/reservas', ReservaController.criarReserva);
 router.get('/runcar/reservas/usuarios', ReservaController.listarReservasUsuarios);
 router.post('/runcar/reservas/:id/pagamento', ReservaController.pagarReservas);
 router.post('/runcar/reservas/:id/cancelar', ReservaController.cancelarReservas);
+router.post('/runcar/reservas/:id/finalizar', ReservaController.finalizarReservas);
 
 export default router;
