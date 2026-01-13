29c888524cf6787022a9724a7386b7068f2d4cad
TaliaMendes <taliafmendes09@gmail.com>
2026-01-13 18:36:17 -0300

    feat: adicion proteção rotas admin com Basic Auth

--- backend/src/routes/Multa.routes.js
+++ backend/src/routes/Multa.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import * as MultaController from '../controllers/MultaController.js';
+import adminAuth from '../middlewares/adminAuth.js';
+
 
 const router = Router();
 
-router.post('/runcar/admin/multas', MultaController.registrarMulta);
+router.post('/runcar/admin/multas', adminAuth, MultaController.registrarMulta);
 router.get('/runcar/multas/reserva/:reservaId', MultaController.listarPorReserva);
 router.get('/runcar/multas/usuarios/:usuario_id', MultaController.listarPorUsuario);
-router.delete('/runcar/admin/multas/:id', MultaController.remover);
+router.delete('/runcar/admin/multas/:id', adminAuth, MultaController.remover);
 
 export default router
