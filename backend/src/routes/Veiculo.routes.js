02b0e1de615bdbeaca05c2aeaa1cfcbc6229f49c
TaliaMendes <taliafmendes09@gmail.com>
2026-01-13 18:36:17 -0300

    feat: adicion proteção rotas admin com Basic Auth

--- backend/src/routes/Veiculo.routes.js
+++ backend/src/routes/Veiculo.routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import * as VeiculoController from '../controllers/VeiculoController.js';
+import adminAuth from '../middlewares/adminAuth.js';
 
 const router = Router();
 
-router.post('/runcar/admin/veiculos', VeiculoController.criar);
-router.get('/runcar/admin/veiculos', VeiculoController.listarVeiculosAdm);
+router.post('/runcar/admin/veiculos', adminAuth, VeiculoController.criar);
+router.get('/runcar/admin/veiculos', adminAuth, VeiculoController.listarVeiculosAdm);
 router.get('/runcar/veiculos', VeiculoController.listarDisponiveis);
-router.put('/runcar/admin/veiculos/:id', VeiculoController.atualizar);
-router.patch('/runcar/admin/veiculos/:id', VeiculoController.inativar);
+router.put('/runcar/admin/veiculos/:id', adminAuth, VeiculoController.atualizar);
+router.patch('/runcar/admin/veiculos/:id', adminAuth,  VeiculoController.inativar);
 
 export default router;
